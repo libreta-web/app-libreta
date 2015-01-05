91bab4e49cfe4dbbf8703733b2040851b50c99b7
Viktorius77 <viktor_viro@hotmail.com>
2015-01-05 18:26:57 -0300

    conexion

--- conexion.php
+++ conexion.php
@@ -1,16 +1,15 @@
 <?php  
 function Conectarse()  
 {  
-   if (!($link=mysql_connect("localhost","root","")))  
+   if (!($link=mysqli_connect("db4free.net","libretaadmin","app-libreta")))  
    {  
       echo "Error conectando a la base de datos.";  
       exit();  
    }  
-   if (!mysql_select_db("agenda",$link))  
+   if (!mysqli_select_db($link,"bdlibreta") ) 
    {  
       echo "Error seleccionando la base de datos.";  
       exit();  
    }  
    return $link;  
 }  
-?>
